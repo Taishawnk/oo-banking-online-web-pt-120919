7df82e707d66109cc0a27834636527f971715ccf
Taishawn King <taishawnking1@yahoo.com>
2020-01-24 03:47:31 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status!="Transaction rejected. Please check your account balance."
+    if @sender.balance > @amount && @status== !="Transaction rejected. Please check your account balance."
       @sender.deposit(@amount*-1)
       @receiver.deposit(@amount)
       @status="complete"
